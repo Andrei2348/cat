4a7cee3dd917cfa43249e2e63487138bd1c98250
Andrei2348 <makarevichandco@gmail.com>
2023-11-27 18:31:00 +0300

    JS

--- js/main.js
+++ js/main.js
@@ -1,19 +1,26 @@
-// const boxes = Array.from(document.querySelectorAll(".box"));
+const boxes = Array.from(document.querySelectorAll('.open'));
 
-// boxes.forEach((box) => {
-//   box.addEventListener("click", boxHandler);
-// });
+boxes.forEach((box) => {box.addEventListener("click", boxHandler)});
 
-// function boxHandler(e) {
-//   e.preventDefault();
-//   boxes.forEach((box) => {box.classList.remove('active')});
+function boxHandler(e) {
+  e.preventDefault();
+  boxes.forEach((box) => {box.classList.remove('active')});
 
-//   let currentBox = e.target.closest(".box");
-//   let currentContent = e.target.nextElementSibling;
-//   currentBox.classList.toggle("active");
-//   if (currentBox.classList.contains("active")) {
-//     currentContent.style.maxHeight = currentContent.scrollHeight + "px";
-//   } else {
-//     currentContent.style.maxHeight = 0;
-//   }
-// }
+  // let currentBox = e.target.closest(".box");
+  // console.log(currentBox)
+
+
+
+
+  let currentContent = e.target.nextElementSibling;
+  console.log(currentContent)
+  currentContent.style.maxHeight = currentContent.scrollHeight + "px";
+
+
+  // currentBox.classList.toggle("active");
+  // if (currentBox.classList.contains("active")) {
+  //   currentContent.style.maxHeight = currentContent.scrollHeight + "px";
+  // } else {
+  //   currentContent.style.maxHeight = 0;
+  // }
+}

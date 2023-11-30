8a4d5d39a3792424b1385d82a137d4b243d12d09
Andrei2348 <makarevichandco@gmail.com>
2023-11-30 20:27:21 +0300

    футер и категории

---
+++ js/slick.js
@@ -0,0 +1,10 @@
+$('.categories__items').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+    focusOnSelect: true,
+    rows: 1,
+    
+});
